6b5bbf3b6c22d806cd6c10b6089094f695b857d8
jonesim <ian@cobi.co.uk>
2022-01-14 13:21:30 +0000

    Add backup of all schemas and options to exclude folders

--- gdrive_backup/backup.py
+++ gdrive_backup/backup.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from encrypted_credentials import django_credentials
 from .backup_db import BackupDb
 from .backup_local_files import BackupLocal
+from .sql_functions import get_schemas
+
 try:
     from .backup_s3 import BackupS3
 except ImportError:
@@ -29,18 +31,21 @@ class Backup:
                         schema=schema,
                         table=table)
 
-    def backup_db_and_folders(self, schema=None, table=None):
-        db = self.get_backup_db(schema, table)
-        db.backup_db_gdrive()
+    def backup_db_and_folders(self, schema=None, table=None, include_db=True, all_schemas=False,
+                              include_folders=True, include_s3_folders=True):
+        if include_db:
+            schemas = [s[0] for s in get_schemas()] if all_schemas else [schema]
+            for s in schemas:
+                db = self.get_backup_db(s, table)
+                db.backup_db_gdrive()
+                db.prune_old_backups(settings.BACKUP_DB_RETENTION)
 
-        db.prune_old_backups(settings.BACKUP_DB_RETENTION)
-
-        if hasattr(settings, 'BACKUP_DIRS'):
+        if include_folders and hasattr(settings, 'BACKUP_DIRS'):
             b = BackupLocal(django_credentials.get_credentials('drive'), settings.BACKUP_GDRIVE_DIR, self.logger)
             for backup in settings.BACKUP_DIRS:
                 b.backup_to_drive(*backup)
 
-        if hasattr(settings, 'S3_BACKUP_DIRS'):
+        if include_s3_folders and hasattr(settings, 'S3_BACKUP_DIRS'):
             s3_backup = BackupS3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY,
                                  django_credentials.get_credentials('drive'),
                                  settings.BACKUP_GDRIVE_DIR,
