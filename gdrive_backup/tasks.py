981f8a25acc1cbed85bd4fd1bb95423a64a1b208
jonesim <ian@cobi.co.uk>
2022-01-14 10:28:15 +0000

    Change logging in tasks

--- gdrive_backup/tasks.py
+++ gdrive_backup/tasks.py
@@ -3,39 +3,37 @@ import logging
 from celery import shared_task
 from .backup import Backup
 
+logger = logging.getLogger(__name__)
+
 
 @shared_task
 def backup():
     Backup().backup_db_and_folders()
 
 
+class StateLogger:
+
+    def __init__(self, task):
+        self.task = task
+
+    def info(self, text):
+        logger.info(text)
+        self.task.update_state(state='PROGRESS', meta={'message': text})
+
+
 try:
     from ajax_helpers.utils import ajax_command
-
-    class EventLogHandler(logging.Handler):
-        def __init__(self, task):
-            logging.Handler.__init__(self)
-            self.task = task
-
-        def emit(self, record):
-            self.task.update_state(state='PROGRESS', meta={'message': record.msg})
-
-    def get_logger(task):
-        logger = logging.getLogger(__name__)
-        handler = EventLogHandler(task)
-        logger.addHandler(handler)
-        return logger
 
     @shared_task(bind=True)
     def ajax_backup(self, schema=None, table=None, **kwargs):
         if 'slug' in kwargs:
             schema = kwargs['slug'].get('pk')
-        Backup(get_logger(self)).backup_db_and_folders(schema=schema, table=table)
+        Backup(StateLogger(self)).backup_db_and_folders(schema=schema, table=table)
         return {'commands': [ajax_command('message', text='Backup Complete'), ajax_command('reload')]}
 
     @shared_task(bind=True)
     def ajax_restore(self, *, slug, **_kwargs):
-        Backup(get_logger(self)).get_backup_db().restore_gdrive_db(file_id=slug['pk'])
+        Backup(StateLogger(self)).get_backup_db().restore_gdrive_db(file_id=slug['pk'])
         return {'commands': [ajax_command('message', text='Restore Complete'), ajax_command('reload')]}
 
 except ModuleNotFoundError:
