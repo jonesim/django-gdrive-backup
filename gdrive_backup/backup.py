a3481ba2fd6a49a2421de8e9a91d939d6d2cb62c
Tom Turner <tom@twt.me.uk>
2025-06-09 21:42:44 +0100

    More logging

--- gdrive_backup/backup.py
+++ gdrive_backup/backup.py
@@ -31,12 +31,15 @@ class Backup:
         if include_db:
             schemas = [s[0] for s in get_schemas()] if all_schemas else [schema]
             for s in schemas:
-
+                self.logger.info(f'Backing up db {s}')
                 db = self.get_backup_db(s, table, sub_folder)
                 db.backup_db_and_upload()
                 if not sub_folder:
+                    self.logger.info('pruning old backups')
                     db.prune_old_backups(settings.BACKUP_DB_RETENTION)
                     self.logger.info('pruned old backups')
+                self.logger.info('Finished schema')
+            self.logger.info('Finished all schemas')
 
         if include_folders and hasattr(settings, 'BACKUP_DIRS'):
             b = BackupLocal(settings.BACKUP_STORAGE_DIR, self.logger)
